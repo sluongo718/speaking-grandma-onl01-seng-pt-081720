f773cf99ddb84f8a66fb0662bb43acec395a28a4
sam l <sluongo718@yahoo.com>
2020-08-20 01:47:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,6 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
-    elsif 
-    return "I LOVE YOU TOO PUMPKIN! "
   else
     return "NO, NOT SINCE 1938!"
   end
